65ecc6e46b09c6895c2e6f214c746a5732d1094d
gijoomoon <gj.moon@outlook.com>
2024-11-01 21:45:58 +0900

    새로운 테스트

--- core/src/main/java/oop/pcg2d/Test.java
+++ core/src/main/java/oop/pcg2d/Test.java
@@ -2,17 +2,23 @@
 
 package oop.pcg2d;
 
+import java.util.Random;
+
 import oop.pcg2d.generator.CellularAutomata;
 
 public class Test {
     public static void main(String[] args) {
         int[][] map = {
-            {0,0,1,0,0,0},
-            {0,1,0,0,0,0},
-            {1,0,0,0,0,0},
-            {0,0,0,0,1,1},
-            {0,0,0,0,1,0},
+            {0,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,0},
         };
-        new CellularAutomata(6, 5, 0, false, 0L).test(map);
+        long seed = new Random().nextLong();
+        CellularAutomata gen = new CellularAutomata(6, 5, 0, false, seed);
+        gen.test(map);
+        gen.testRender();
+
     }
 }
