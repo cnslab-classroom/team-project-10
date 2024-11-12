f3a16edc9cd1bfb61b99cc339e650bbae717254e
KIMJUNSIK <wnstlr0830@kw.ac.kr>
2024-11-12 22:19:26 +0900

    각종 스크린 구현 및 맵 프로그램 실행

--- core/src/main/java/oop/pcg2d/App.java
+++ core/src/main/java/oop/pcg2d/App.java
@@ -1,55 +1,75 @@
 package oop.pcg2d;
-import oop.pcg2d.generator.CellularAutomata;
-import oop.pcg2d.painter.Painter;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import oop.pcg2d.Screen.*;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.FPSLogger;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class App extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture[] grass;
-    private CellularAutomata ca = new CellularAutomata(100, 100, 0.45, false, 1);
-    private Painter painter = new Painter();
-    private int[][] map = ca.generate();
-    
+// Game 클래스는 여러 화면을 관리하고 게임 루프를 실행하는 클래스
+public class App extends Game {
+    // 로그 기록
+    public static String LOG = App.class.getSimpleName();
+
+    private FPSLogger fpsLogger; // fps 로그
+
+    public static boolean DEV_MODE = true; // 개발 모드
+
+    private MainMenuScreen mainMenuScreen; // 메인 메뉴 화면 인스턴스
+
+
     @Override
+    // 게임이 시작될 때 호출되는 메서드
     public void create() {
-        batch = new SpriteBatch();
-        grass = new Texture[9];
-        grass[0] = new Texture("grass_left_top.png");
-        grass[1] = new Texture("grass_mid_top.png");
-        grass[2] = new Texture("grass_right_top.png");
-        grass[3] = new Texture("grass_left_mid.png");
-        grass[4] = new Texture("grass_mid_mid.png");
-        grass[5] = new Texture("grass_right_mid.png");
-        grass[6] = new Texture("grass_left_bot.png");
-        grass[7] = new Texture("grass_mid_bot.png");
-        grass[8] = new Texture("grass_right_bot.png");
+        Gdx.app.log(LOG, "Creating game on " + Gdx.app.getType());
+        this.fpsLogger = new FPSLogger();
+        
+        // 메인메뉴 화면을 생성함. Main클래스 인스턴스를 전달하여 생성
+        this.mainMenuScreen = new MainMenuScreen(this);
+
+        // 현재 화면을 메인메뉴 화면으로 설정함
+        this.setScreen(this.mainMenuScreen);
     }
 
     @Override
+    // 화면 크기가 변경될 때 호출되는 메서드
+    public void resize(int width, int height) {
+
+        // 현재 활성화된 화면의 resize 메서드 호출
+        super.resize(width, height);
+
+        Gdx.app.log(LOG, "Resizing game to: " + width + " x " + height);
+    }
+
+    @Override
+    // 매 프레임마다 호출되는 메서드
     public void render() {
-        ScreenUtils.clear(Color.BLACK);
-        batch.begin();
-        painter.draw(map, batch, grass);
-        batch.end();
+
+        // 현재 활성화된 화면의 render 메서드 호출
+        super.render();
+
+        // 개발 모드일 때 fps 로그 출력
+        if (DEV_MODE) {
+            this.fpsLogger.log();
+        }
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+        Gdx.app.log(LOG, "Pausing game");
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+        Gdx.app.log(LOG, "Resuming game");
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        for(int i = 0; i < 9; i++) {
-            grass[i].dispose();
-        }
+        super.dispose();
+        Gdx.app.log(LOG, "Disposing game");
+        this.mainMenuScreen.dispose();
     }
-
     
 }
-
---
+++ core/src/main/java/oop/pcg2d/Screen/MapGenerationScreen.java
@@ -0,0 +1,156 @@
+package oop.pcg2d.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import oop.pcg2d.generator.CellularAutomata;
+import oop.pcg2d.generator.RoomsAndMazes;
+import oop.pcg2d.painter.Painter;
+import oop.pcg2d.App;
+
+public class MapGenerationScreen extends AbstractScreen {
+
+    private int mapWidth;
+    private int mapHeight;
+    private long seed;
+    private String algorithm;
+
+    // 셀룰러 오토마타 파라미터
+    private double fillProb;
+    private boolean isConnected;
+
+    // Rooms and Mazes 파라미터
+    private int roomMinLen;
+    private int roomMaxLen;
+    private int roomGenAttempt;
+    private boolean removeDeadend;
+
+    private int[][] mapData; // 생성된 맵 데이터
+
+    private SpriteBatch batch;
+    private Texture[] tileTextures;
+    private Painter painter;
+
+    // 뒤로가기 버튼 변수
+    private TextButton backButton;
+
+    // 생성자 (셀룰러 오토마타 알고리즘용)
+    public MapGenerationScreen(App game, int mapWidth, int mapHeight, long seed, double fillProb, boolean isConnected) {
+        super(game);
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+        this.seed = seed;
+        this.algorithm = "Cellular Automata";
+        this.fillProb = fillProb;
+        this.isConnected = isConnected;
+
+        init();
+    }
+
+    // 생성자 (Rooms and Mazes 알고리즘용)
+    public MapGenerationScreen(App game, int mapWidth, int mapHeight, long seed, int roomMinLen, int roomMaxLen,
+            int roomGenAttempt, boolean removeDeadend) {
+        super(game);
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+        this.seed = seed;
+        this.algorithm = "Rooms and Mazes";
+        this.roomMinLen = roomMinLen;
+        this.roomMaxLen = roomMaxLen;
+        this.roomGenAttempt = roomGenAttempt;
+        this.removeDeadend = removeDeadend;
+
+        init();
+    }
+
+    private void init() {
+        batch = new SpriteBatch();
+
+        // 타일 텍스처 로드 (팀원들과 동일한 경로와 이름으로 설정)
+        tileTextures = new Texture[9];
+        tileTextures[0] = new Texture(Gdx.files.internal("grass_left_top.png"));
+        tileTextures[1] = new Texture(Gdx.files.internal("grass_mid_top.png"));
+        tileTextures[2] = new Texture(Gdx.files.internal("grass_right_top.png"));
+        tileTextures[3] = new Texture(Gdx.files.internal("grass_left_mid.png"));
+        tileTextures[4] = new Texture(Gdx.files.internal("grass_mid_mid.png"));
+        tileTextures[5] = new Texture(Gdx.files.internal("grass_right_mid.png"));
+        tileTextures[6] = new Texture(Gdx.files.internal("grass_left_bot.png"));
+        tileTextures[7] = new Texture(Gdx.files.internal("grass_mid_bot.png"));
+        tileTextures[8] = new Texture(Gdx.files.internal("grass_right_bot.png"));
+
+        painter = new Painter();
+
+        // 맵 생성
+        generateMap();
+    }
+
+    private void generateMap() {
+        if (algorithm.equals("Cellular Automata")) {
+            CellularAutomata ca = new CellularAutomata(mapWidth, mapHeight, fillProb, isConnected, seed);
+            mapData = ca.generate();
+        } else if (algorithm.equals("Rooms and Mazes")) {
+            RoomsAndMazes rm = new RoomsAndMazes(seed, mapWidth, mapHeight, roomMinLen, roomMaxLen, roomGenAttempt,
+                    removeDeadend);
+            mapData = rm.generate();
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        // 기존 코드 유지...
+
+        // 뒤로 가기 버튼 생성
+        backButton = new TextButton("Back", skin);
+
+        // 버튼에 클릭 리스너 추가
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // SelectMap 화면으로 돌아가기
+                game.setScreen(new SelectMap(game));
+                dispose(); // 현재 화면 자원 해제
+            }
+        });
+
+        // 버튼을 스테이지에 추가
+        Table table = new Table();
+        table.setFillParent(true);
+        table.top().left().pad(10);
+        table.add(backButton).width(100).height(50);
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void render(float delta) {
+        // 화면을 지움
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // 맵 렌더링 코드 유지
+        batch.begin();
+        painter.draw(mapData, batch, tileTextures);
+        batch.end();
+
+        // 스테이지를 업데이트하고 그리기
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        // 리소스 해제
+        batch.dispose();
+        for (Texture texture : tileTextures) {
+            texture.dispose();
+        }
+    }
+}
